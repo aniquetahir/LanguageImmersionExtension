376c04bff593d483e065777d37d6638c26246c42
Anique Rogers Tahir <aniquetahir@gmail.com>
2017-11-29 00:08:52 -0700

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    entry: './src/app.js',
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            }
+        ]
+    },
+    output: {
+        filename: 'immerse.js'
+    }
+};
